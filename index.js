138fb661e306c8b97fb967e579308f15bae4291e
Nate Geslin <teamtomkins23@gmail.com>
2018-03-31 18:01:34 -0500

    Initialize

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ *
+ *
+ * -e|--entry       Entry point, can be file or directory. If empty, assumes current working directory
+ * -o|--output      Result file ends up here
+ *
+ * --use-require    Looks for node-style `require` (default: false)
+ * --use-import     Looks for ES2015+ `import`  (default: true)
+ */
+const lumberjack = require('./lib/lumberjack');
+
+
